f6dc8f806eea5ea790519349e6ac680279596800
Guillaume riedinger <guillaume.riedinger@weibis-nord.fr>
2022-12-04 20:10:08 +0100

    fixing git ignore

---
+++ GlobalRank/GlobalRank.Infrastructure/Repositories/RankRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalRank.Infrastructure.Repositories
+{
+    public class RankRepository
+    {
+        private readonly string _jsonPath;
+
+        public RankRepository()
+        {
+            _jsonPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\data.json");
+        }
+    }
+}
