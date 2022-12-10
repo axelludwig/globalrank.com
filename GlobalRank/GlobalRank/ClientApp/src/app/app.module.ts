44fe2670e648384b0d3f5b2b5163314ef6e7c1bc
Guillaume riedinger <guillaume.riedinger@weibis-nord.fr>
2022-12-10 15:31:18 +0100

    Adding angular material

--- GlobalRank/GlobalRank/ClientApp/src/app/app.module.ts
+++ GlobalRank/GlobalRank/ClientApp/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +29,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
